da77dc15700c3cd2535f0b2ec944e18d20791679
violeta070601 <yanet01hernandez@gmail.com>
2025-06-12 02:43:46 -0600

    Describe brevemente el cambio que hiciste

--- tiendaVirtual/build.sh
+++ tiendaVirtual/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Build commands for Render
+
+set -o errexit
+pip install -r requirements.txt
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
